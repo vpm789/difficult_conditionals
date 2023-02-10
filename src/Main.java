5d6db9edcdebf24e4444a309879285cfb3df3931
vpm789 <vpm789@gmail.com>
2023-02-10 23:09:27 +0300

    task6-7

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,34 @@ public class Main {
     public static void task6() {
         System.out.println("");
         System.out.println("Задача №6");
+        int age = 36;
+        int salary = 250000;
+        double creditLimit = 0;
+        if (age >= 23) {
+            if (salary < 50000) {
+                creditLimit = salary * 3;
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + creditLimit + " рублей");
+            } else if (salary >= 50000 && salary < 80000) {
+                creditLimit = salary * 3 * 1.2;
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + creditLimit + " рублей");
+            } else {
+                creditLimit = salary * 3 * 1.5;
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + creditLimit + " рублей");
+            }
+        } else {
+            if (salary < 50000) {
+                creditLimit = salary * 2;
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + creditLimit + " рублей");
+            } else if (salary >= 50000 && salary < 80000) {
+                creditLimit = salary * 2 * 1.2;
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + creditLimit + " рублей");
+            } else {
+                creditLimit = salary * 2 * 1.5;
+                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + creditLimit + " рублей");
+            }
+        }
     }
+
     public static void task7() {
         System.out.println("");
         System.out.println("Задача №7");
